f8676caabc21e4c52fc31c728f0494f8b0757da0
jared <jaredrjack93@gmail.com>
2023-05-31 19:31:33 -0400

    updated code

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -31,7 +31,7 @@ module.exports = () => {
         inject: true,
         name: "Just Another Text Editor",
         short_name: "J.A.T.E",
-        description: ""
+        description: "",
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "/",
@@ -42,7 +42,7 @@ module.exports = () => {
             size: [96, 128, 192, 256, 384, 512],
             destination: path.join("assets", "icons"),
           },
-        ].
+        ],
       }),
     ],
 
@@ -50,7 +50,19 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: /node_modules/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+        
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
         },
       ],
     },
